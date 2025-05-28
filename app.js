b56acada7577beb692cf62c57a7570eb3ba4884c
Jose <jm.bebor@gmail.com>
2025-05-28 10:30:24 +0000

    [feat]Funciones de contador y globos explotando correctos

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 // we declare a new global variable containing an array that represents the ballons map
 // you have to add more colors into the ballonsMap array
-let ballonsMap = ['green'];
-
+const ballonsMap = ['green', 'red', 'blue', 'yellow', 'pink', 'purple', 'orange', 'grey', 'orange', 'coral', 'green', 'red', 'blue', 'yellow', 'pink', 'purple', 'orange', 'grey', 'orange', 'coral'];
+const activeBalloons = ballonsMap.filter(color => color != null).length
 // poping a balloon is basically turning his color to null (no color)
 const popBalloon = (position) => {
     // set the color to null on the balloon position
+    ballonsMap[position] = "null";
+    activeBalloons--;
     render();
 }
 
+
+
 const render = () => {
-    
+
     // convert ballons map of colors into real html balloons
     const ballons = ballonsMap.map((color, position) => {
-        return `<div class="balloon active"></div>`; // <--- render each balloon
+        if (color === "null") return `<div class="balloon popped" id="${position}" style="background-color:${color}"></div>`;
+        return `<div class="balloon active" id="${position}" style="background-color:${color}"></div>`; // <--- render each balloon
     });
-
-    document.querySelector("#balloon-count").innerHTML = ballons.filter(b => b !== null).length; // <-- render the balloon count into the DOM
     document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
+    ballonsMap.forEach((color, position) => {
+        if (color != null) {
+            const element = document.getElementById(position);
+            if (element) {
+                element.addEventListener("click", () => popBalloon(position))
+            }
+        }
+    })
 
-    if(activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
+
+
+
+    document.querySelector("#balloon-count").innerHTML = activeBalloons // <-- render the balloon count into the DOM
+    if (activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
 }
 
 // this makes the "render" function trigger when the website starts existing
